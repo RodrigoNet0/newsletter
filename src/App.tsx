0a107b1411ad50703407caa7d580fae4b3c7cb5f
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-12-10 19:56:07 +0000

    commit init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+     <h1 className="text-red-600">Inscreva-se</h1>
+     <Form />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Form = () => {
+    return(
+        <div>Form</div>
+    );
+   
+}
+
+export default Form
